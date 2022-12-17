111c365c8663df325634150b9f6b8009e3120409
NishatFariza <nishatfarzia78@gmail.com>
2022-12-17 13:05:37 +0600

    wip:reset all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { LoginForm } from '@/components/LoginForm'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -6,7 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="wrapper">
-      <LoginForm />
+     
     </div>
   );
 }
