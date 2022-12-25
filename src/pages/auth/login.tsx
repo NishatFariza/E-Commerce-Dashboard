cd1cfcb042a4f3d8d236965f38346d39566f91d8
NishatFariza <nishatfarzia78@gmail.com>
2022-12-25 16:09:37 +0600

    react hook form validation done

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,52 +1,74 @@
+import * as yup from "yup";
 import { Anchor, Button, Input, Paper, Space, Title } from "@mantine/core";
-
 import { IconLock, IconMail } from "@tabler/icons";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const validationSchema = yup.object({
+  email: yup.string().required("Required").email("Invalid Email"),
+  password: yup.string().required().min(6).max(30),
+});
 
 const LoginPage = () => {
-  const { register, handleSubmit, setValue, watch } = useForm({
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
     defaultValues: {
       email: "Nisat",
       password: "pass",
     },
   });
 
-  const handleSubmitForm = () => {
-    // console.log({ email, password });
+  const handleSubmitForm = (values: any) => {
+    console.log(values);
+    //root: api call
   };
 
   return (
     <div className="bg-slate-100 h-screen">
       <div className="max-w-md mx-auto py-20">
         <Paper shadow="lg" radius="md" p="md">
-          <form action="#" onSubmit={handleSubmitForm}>
+          <form action="#" onSubmit={handleSubmit(handleSubmitForm)}>
             <pre>{JSON.stringify(watch("email"), null, 2)}</pre>
             <pre>{JSON.stringify(watch("password"), null, 2)}</pre>
+            <pre>
+              {JSON.stringify({ error: errors?.email?.message }, null, 2)}
+            </pre>
+            <pre>
+              {JSON.stringify({ error: errors?.password?.message }, null, 2)}
+            </pre>
 
             <Title order={4} color={"dark.3"}>
               Access To Your Account
             </Title>
             <Space h={"md"} />
-            <Input.Wrapper withAsterisk label="Email">
+            <Input.Wrapper
+              withAsterisk
+              label="Email"
+              error={errors?.email?.message}
+            >
               <Input
-                required
-                type={"email"}
-                value={watch("email")}
-                onChange={(e) => setValue("email", e.target.value)}
                 icon={<IconMail size={16} />}
                 placeholder="Your email"
+                {...register("email")}
               />
             </Input.Wrapper>
             <Space h={"sm"} />
-            <Input.Wrapper withAsterisk label="Password">
+            <Input.Wrapper
+              withAsterisk
+              label="Password"
+              error={errors?.password?.message}
+            >
               <Input
-                required
                 type={"password"}
-                value={watch("email")}
-                onChange={(e) => setValue("password", e.target.value)}
                 icon={<IconLock size={16} />}
                 placeholder="Enter your password"
+                {...register("password")}
               />
             </Input.Wrapper>
             <Space h={"sm"} />
