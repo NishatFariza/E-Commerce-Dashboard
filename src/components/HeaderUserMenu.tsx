74774189c2a0f973b371952436b9b13ee6937bc5
NishatFariza <nishatfarzia78@gmail.com>
2023-01-04 17:29:46 +0600

    logout api done

--- src/components/HeaderUserMenu.tsx
+++ src/components/HeaderUserMenu.tsx
@@ -1,26 +1,34 @@
 import { AppContext } from "@/context/AppContextProvider";
 import { Avatar, Menu } from "@mantine/core";
 import { IconLogout, IconSettings } from "@tabler/icons";
-import  { useContext } from "react";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 
 const HeaderUserMenu = () => {
+  const { user } = useContext(AppContext);
+  const router = useRouter();
 
- const context = useContext(AppContext)
+  const handleLogOut = () => {
+    localStorage.removeItem("Token");
+    router.push("/auth/login");
+  };
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
         <Avatar
           radius={"lg"}
-          src="https://i.pinimg.com/originals/bc/e0/87/bce087eed31aafac6189c21ab0f859f2.jpg"
-          alt="it's me"
+          src={`http://104.251.211.125:8055/assets/${user?.avatar}?width=80`}
+          alt={user?.first_name}
         />
       </Menu.Target>
 
       <Menu.Dropdown>
         {/* @ts-ignore */}
-        <Menu.Label>Application { context.x}</Menu.Label>
+        <Menu.Label>Application</Menu.Label>
         <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
-        <Menu.Item icon={<IconLogout size={14} />}>Logout</Menu.Item>
+        <Menu.Item onClick={handleLogOut} icon={<IconLogout size={14} />}>
+          Logout
+        </Menu.Item>
       </Menu.Dropdown>
     </Menu>
   );
