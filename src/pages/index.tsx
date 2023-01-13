7c1f2b08db7b63e4ad8e814f578bc4be3dd65aa4
NishatFariza <nishatfarzia78@gmail.com>
2023-01-13 08:39:57 +0600

    wip: modal image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,39 @@
 import productRepo from "@/app/api/repositories/product.repo";
 import AdminLayout from "@/components/AdminLayout";
-import { Button, Paper, Space, Table, Title, Image } from "@mantine/core";
+import {
+  Button,
+  Paper,
+  Space,
+  Table,
+  Title,
+  Image,
+  Group,
+  Modal,
+} from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
   const { isLoading, data } = useQuery(["products"], () => {
     return productRepo.getProducts({ limit: 10, page: 1 });
   });
 
+  const [opened, setOpened] = useState(false);
+  const [modalImage, setModalImage] = useState<string | null>(null);
+
   return (
     <AdminLayout>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title={"Product Image"}
+      >
+        <Image
+          src={`http://104.251.211.125:8055/assets/${modalImage}`}
+          radius={"sm"}
+        />
+      </Modal>
       <div className="flex justify-between items-center">
         <Title order={3} color={"dark.4"}>
           Products
@@ -35,19 +58,23 @@ export default function Home() {
                 <td>{product.id}</td>
                 <td>{product.title}</td>
                 <td>{product.price}</td>
-                <td>
+                <td
+                  onClick={() => {
+                    setOpened(true), setModalImage(product.cover_image);
+                  }}
+                  className="cursor-pointer"
+                >
                   <Image
                     src={`http://104.251.211.125:8055/assets/${product.cover_image}?width=60`}
                     radius={"sm"}
                     alt={product.title}
-                   
-                    
                   />
                 </td>
-                <td className="flex gap-2 items-center justify-center ">
-                  <Button>Edit</Button>
-
-                  <Button color={"red.5"}>Action</Button>
+                <td>
+                  <Group position="center">
+                    <Button>Edit</Button>
+                    <Button color={"red.5"}>Action</Button>
+                  </Group>
                 </td>
               </tr>
             ))}
